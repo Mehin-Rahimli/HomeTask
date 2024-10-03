748efff44e5292d55223ba6f565cf11c0e02c8db
Mehin-Rahimli <mehincr-bp217@code.edu.az>
2024-10-03 01:47:58 -0700

    initial commit

---
+++ Program2/Program2/Program.cs
@@ -0,0 +1,27 @@
+﻿//Verilmis ededin sade ve ya murekkeb oldugunu yoxlayin.
+int num= 8;
+if (num < 2)
+{
+    Console.WriteLine("Ne sade ne de murekkeb ededdir.");
+}
+else if (num >= 2)
+{
+    for (int i = 2; i < num; i++)
+    {
+        if (num % i == 0)
+        {
+            Console.WriteLine("Bu eded murekkebdir");
+
+
+        }
+        else
+        {
+            Console.WriteLine("bu eded sadedir");
+
+        }
+    }
+}
+else
+{
+    Console.WriteLine("Duzgun input deyil");
+}
