cdf2f991abba2df1bef8d9d1fc52d27aa06b7586
Mehin-Rahimli <mehincr-bp217@code.edu.az>
2024-10-02 23:15:01 -0700

    Initial commit

---
+++ Task1/Program.cs
@@ -0,0 +1,15 @@
+﻿//Verilmis 2 mertebeli ededin en boyuk reqemini tapin.
+int num = 25;
+
+if (num % 10 > num / 10)
+{
+    Console.WriteLine(num % 10);
+}
+else if (num % 10 < num / 10)
+{
+    Console.WriteLine(num / 10);
+}
+else
+{
+    Console.WriteLine("They are equal");
+}
