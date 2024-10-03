1564bfa0af7a5d36e9bf3a021e83d3d1ff4e2920
Mehin-Rahimli <mehincr-bp217@code.edu.az>
2024-10-03 01:37:47 -0700

    initial commit

---
+++ task3/task3/Program.cs
@@ -0,0 +1,28 @@
+﻿//Verilmis ededin 2nin quvveti olub olmadigini yoxlayin
+float num = 32f;
+if (num % 2 != 0 && num < 2)
+{
+    Console.WriteLine("2nin quvveti deyil");
+}
+
+else if (num >= 2)
+{
+    do
+    {
+
+        num = num / 2;
+    } while (num > 1);
+    if (num == 1)
+    {
+        Console.WriteLine("2nin quvvetidir");
+    }
+    else
+    {
+        Console.WriteLine("2nin quvveti deyil");
+    }
+
+}
+else
+{
+    Console.WriteLine("2nin quvveti deyil");
+}
